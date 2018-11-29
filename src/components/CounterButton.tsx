246c1ec1d9c1f31d029f87ed1467e56e99f7b33a
carlamendozadominguez <carlamendozadominguez@gmail.com>
2018-11-29 18:08:38 +0000

    Arreglando test

--- src/components/CounterButton.tsx
+++ src/components/CounterButton.tsx
@@ -10,12 +10,16 @@ export default class CounterButton extends React.Component<{}, CounterButtonStat
         this.state = {
             numberOfClicks: 0
         };
+ 
+        this.incrementCount = this.incrementCount.bind(this);
+
     }
-    incrementCount(){
+    incrementCount() {
         this.setState({
             numberOfClicks: this.state.numberOfClicks + 1
         });
     }
+    
     render() {
     return <button onClick={this.incrementCount}>{this.state.numberOfClicks}</button>;
   }
