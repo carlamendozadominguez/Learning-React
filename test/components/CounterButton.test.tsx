450c5bd7e72ab77c75b0aeaf0ce25703a3db7321
carlamendozadominguez <carlamendozadominguez@gmail.com>
2018-11-29 20:47:43 +0000

    Metidos los test en la carpeta component

--- test/components/CounterButton.test.tsx
+++ test/components/CounterButton.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import CounterButton from "../src/components/CounterButton";
+import CounterButton from "../../src/components/CounterButton";
 
 describe('CounterButton', () => {
  describe('render', () => {
