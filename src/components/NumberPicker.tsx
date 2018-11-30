008fb9ae6a399a3da9d454a14700b3fd930516b7
carlamendozadominguez <carlamendozadominguez@gmail.com>
2018-11-30 17:37:17 +0000

    Puesto el css en el componente no en un fichero separado

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -1,5 +1,46 @@
 import * as React from "react";
 
+const numberPickerStyle: React.CSSProperties = {
+  width: '150px',
+  height: '32px',
+  display: 'flex',
+  alignItems: 'center',
+  position: 'relative',
+  border: '1px solid #EDEEF4',
+  boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.03)',
+  color: '#385871',
+  borderRadius: '5px'
+ };
+ 
+ const numberPickerLabel: React.CSSProperties = {
+  width: '100px',
+  height: '32px',
+  textAlign: 'center',
+  padding: '0',
+  outline: 'none',
+  fontFamily: 'OpenSans-Bold, sans-serif',
+  color: '#385871',
+  borderRadius: '5px',
+  lineHeight: '30px'
+ };
+ 
+ const numberPickerButton: React.CSSProperties = {
+  backgroundColor: "#4CAF50",
+  border: "none",
+  color: "white",
+  width: "30px",
+  height: "30px",
+  textAlign: "center",
+  textDecoration: "none",
+  display: "inline-block",
+  borderRadius: "50%",
+  fontSize: "25px",
+  paddingBottom: "5px",
+  lineHeight: "25px",
+  cursor: "pointer",
+  outline: 'none'
+ }; 
+
 export interface NumberPickerState {
   counter: number;
 }
@@ -28,10 +69,16 @@ export default class NumberPicker extends React.Component<{}, NumberPickerState>
   }
 
   render() {
-    return <div className="number-picker">
-      <button className="number-picker-buttons" onClick={this.increaseCounter}>+</button>
-      <span className="number-picker-label">{this.state.counter}</span>
-      <button className="number-picker-buttons" onClick={this.decreaseCounter}>-</button>
-    </div>;
+    return (
+      <div style={numberPickerStyle}>
+        <button style={numberPickerButton} onClick={this.increaseCounter}>
+            +
+        </button>
+        <span style={numberPickerLabel}>{this.state.counter}</span>
+        <button style={numberPickerButton} onClick={this.decreaseCounter}>
+            -
+        </button>
+      </div>
+    );
   }
-}
+ }
